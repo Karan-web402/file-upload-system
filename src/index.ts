f9c2dfd765ab70b0d843683b3f38c44a65955c61
Karan Singh <karan.singh@punon.in>
2025-04-09 18:11:44 +0530

    feat: add env file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { app } from "./app";
+import dotenv from 'dotenv'; 
+dotenv.config();
 
-const port = 2424
+const port = process.env.PORT
+
+
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
---
+++ test.js
@@ -0,0 +1,13 @@
+console.log("1");
+
+setTimeout(() => {
+    console.log("first time out")
+},10000)
+
+console.log("2");
+
+setTimeout(() => {
+    console.log("first time out 2")
+},4000)
+
+
